b561fb7392e2f5b15cf84e1115eb8f5b95cbefac
Daniel Barcelona <daniel.barcelona@urv.cat>
2018-07-10 09:27:29 +0200

    version

--- pyactor/__init__.py
+++ pyactor/__init__.py
@@ -5,6 +5,6 @@ PyActor: Python Actorlibrary
 __all__ = ['version_info', 'version']
 
 #: Version information ``(major, minor, revision)``.
-version_info = (0, 0, 1)
+version_info = (1, 4, 0)
 #: Version string ``'major.minor.revision'``.
 version = '.'.join(map(str, version_info))
